a320a38b4d52a8090509f0e58b7ef02b3c1a3f66
nikhilsoniii <162472512+nikhilsoniii@users.noreply.github.com>
2024-07-02 10:31:18 +0530

    Update 9_Gas Station.cpp

--- 9_Gas Station.cpp
+++ 9_Gas Station.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n=gas.size();
+        int netcost=0, temp=0;
+        int index;
+        for(int i=0; i<n; i++){
+            netcost+= (gas[i]-cost[i]);
+             temp += gas[i]-cost[i];
 
+            if(temp<0){
+                index=i+1;
+                temp=0;
+            }
+
+
+        }
+        if(netcost<0) return -1;
+        else if(index>n) return 0;
+        else return index;
+   
+            
+
+
+
+        
+    }
+};
