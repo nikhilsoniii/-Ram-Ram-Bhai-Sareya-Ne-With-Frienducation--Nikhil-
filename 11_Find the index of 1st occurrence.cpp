208a288730a0316adb570433f56136057dfafb9c
nikhilsoniii <162472512+nikhilsoniii@users.noreply.github.com>
2024-07-02 08:39:30 +0530

    Update 11_Find the index of 1st occurrence.cpp

--- 11_Find the index of 1st occurrence.cpp
+++ 11_Find the index of 1st occurrence.cpp
@@ -36,3 +36,29 @@ public:
             
     }
 };
+
+
+
+
+// SECOND APPROACH:
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         int n= haystack.size();
+//         int m= needle.size();
+
+//         if(n<m) return -1;
+//         if(m==0) return 0;
+        
+//       for (int i = 0; i <= n - m; ++i) {
+//             int j = 0;
+//             while (j < m && haystack[i + j] == needle[j]) {
+//                 ++j;
+//             }
+//             if (j == m) {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
